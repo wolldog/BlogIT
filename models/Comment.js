5b7ecdef02e51a071af4af51a979e064fa9e8d44
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-03-15 13:09:03 +1100

    A. model; Comment

---
+++ models/Comment.js
@@ -0,0 +1,35 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    comment_text: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'reader',
+            key: 'id',
+        },
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post',
+  }
+);
+
+module.exports = Comment;

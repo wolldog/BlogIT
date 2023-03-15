fd5c4122de8fd3f078b9f1cfd7cb21cced2dfde4
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-03-15 15:08:27 +1100

    A. home-routes.js; first pass

---
+++ controllers/home-routes.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+const { Post, User } = require('../models');
+
+// GET all posts for homepage
+router.get('/', async (req, res) => {
+  try {
+    const dbPostData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
+    
+    const posts = dbPostData.map((post) =>
+      post.get({ plain: true })
+    );
+    res.render('homepage', {
+      posts,
+      // loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// Login route
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+    res.render('login');
+  });
+  
+  module.exports = router;

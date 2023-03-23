4a8b01179b09dcfc5d6fd1fdf934b47965908e37
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-03-23 18:03:44 +1100

    M.  remove withAuth from onePost route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -29,7 +29,7 @@ router.get("/", async (req, res) => {
 
 // GET one post
 
-router.get("/post/:id", withAuth, async (req, res) => {
+router.get("/post/:id", async (req, res) => {
   try {
     const dbPostData = await Post.findByPk(req.params.id, {
       include: [

b420ec1a08e8729d25e0dc6aa65abd192ee45787
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-03-26 13:10:51 +1100

    M. home-routes.js; add dashboard route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,7 @@
 const router = require("express").Router();
 const { Post, User, Comment } = require("../models");
 // Import the custom middleware
-// const withAuth = require("../utils/auth");
-
+const withAuth = require("../utils/auth");
 
 // GET all posts for homepage
 router.get("/", async (req, res) => {
@@ -21,7 +20,6 @@ router.get("/", async (req, res) => {
       posts,
       loggedIn: req.session.loggedIn,
     });
-
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
@@ -34,32 +32,34 @@ router.get("/post/:id", async (req, res) => {
   try {
     const dbPostData = await Post.findByPk(req.params.id, {
       include: [
-        { model: User, attributes: ["username"]},
-        { model: Comment, where: {post_id: req.params.id}, include: [{ model: User, attributes: ["username"] }], required: false}
-      ]
+        { model: User, attributes: ["username"] },
+        {
+          model: Comment,
+          where: { post_id: req.params.id },
+          include: [{ model: User, attributes: ["username"] }],
+          required: false,
+        },
+      ],
     });
 
     const postData = dbPostData.get({ plain: true });
 
-    const commentData = dbPostData.comments.map((comment) => comment.get({ plain: true }));
+    const commentData = dbPostData.comments.map((comment) =>
+      comment.get({ plain: true })
+    );
 
-  
-
-    
     // console.log(JSON.stringify(postData))
 
-    res.render("onePost", { 
+    res.render("onePost", {
       postData,
-      commentData, 
-      loggedIn: req.session.loggedIn });
-
+      commentData,
+      loggedIn: req.session.loggedIn,
+    });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
 });
-
-
 
 // Login route
 router.get("/login", (req, res) => {
@@ -75,15 +75,25 @@ router.get("/signup", (req, res) => {
   res.render("signup");
 });
 
-// Login route
-router.get("/dashboard", (req, res) => {
- 
-  res.render("dashboard", {
-    loggedIn: req.session.loggedIn,
+// Dashboard route
+router.get("/dashboard", withAuth, async (req, res) => {
+  try {
+    const dbMyPostData = await Post.findAll({
+      where: {
+        user_id: req.session.currentUser,
+      },
+    });
+
+    const myPosts = dbMyPostData.map((myPost) => myPost.get({ plain: true }));
+    console.log(myPosts);
+    res.render("dashboard", {
+      myPosts,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
   }
-  );
 });
-
-
 
 module.exports = router;

c547c6289e3107a90eca402e1c763e1bbacf3f44
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-03-26 13:11:55 +1100

    M. comment-routes.js; implement withAuth

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,17 +1,18 @@
 const router = require("express").Router();
-const { Comment } = require("../../models");
+const { Comment, User } = require("../../models");
+const withAuth = require("../../utils/auth");
 
 // CREATE comment
-router.post("/", async (req, res) => {
+router.post("/", withAuth, async (req, res) => {
 
 
   try {
-    const dbCommentData = await Comment.create({
+    const newCommentData = await Comment.create({
       comment_text: req.body.comment,
       user_id: req.session.currentUser,
       post_id: req.body.postId
     });
-    res.status(200).json(dbCommentData);
+    res.status(200).json(newCommentData);
   } catch (err) {
     console.log(err);
     res.status(500).json(err);

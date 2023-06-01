5be9495ded9f5a1386be0f491829395a00eca5d8
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-06-01 13:06:26 +1000

    M. api/index.js; add postRoutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
 const commentRoutes = require('./comment-routes')
+const postRoutes = require('./post-routes')
 
 router.use('/users', userRoutes);
-router.use('/comment', commentRoutes)
+router.use('/comment', commentRoutes);
+router.use('/post', postRoutes)
 
 module.exports = router;
